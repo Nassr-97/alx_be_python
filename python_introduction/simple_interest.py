9e342650f10e81a9d35e63860d13c9eb0c3e1635
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2024-12-14 12:17:49 +0100

    adding script to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Initializing variable
+principal = 1000
+rate = 0.05
+time = 3
+# Calculating interest
+interest = principal * rate * time
+# Printing the interest value
+print("The simple interest is:", interest)

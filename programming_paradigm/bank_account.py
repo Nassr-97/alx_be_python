aea64cb0203165a52b179fd92084a9b5e85e8eb7
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2025-01-20 21:53:49 +0100

    first

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 
 
 class BankAccount:
-    def __int__(self, account_balance=0):
+    def __init__(self, account_balance=0):
         self.account_balance = account_balance
 
     def deposit(self, amount):

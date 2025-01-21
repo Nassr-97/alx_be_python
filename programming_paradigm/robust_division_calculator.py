07249b109e8bc95dfabd803dd855cf923e63b314
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2025-01-21 09:14:26 +0100

    second

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,10 @@
 
 
 def safe_divide(numerator, denominator):
+
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
         result = numerator / denominator
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
